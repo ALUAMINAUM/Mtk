93b41e092ef5d6e954ba93530b8421b773db1ed4
ALUAMINAUM <149672953+ALUAMINAUM@users.noreply.github.com>
2023-11-18 12:44:48 +0700

    Create Mtk.py

---
+++ Mtk.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import time
+import storage
+import random
+import socket
+
+os.system("clear")
+print "Sedang Loading Database..."
+time.sleep(4)
+print "Menjalankan Sistem Operasi..."
+time.sleep(4)
+os.system("clear")
+os.system("figlet Matematika Linux")
+print "===================================="
+print "= AUTHOR  = BY MATA ELANG          ="
+print "= TEAM    = HACKING RI KULONPROGO  ="
+print "= SC      = ALUAMINAUM/Matematika  ="
+print "===================================="
+
+next=raw_input("Ketik Lanjut Untuk Memulai= ")
+time.sleep(4)
+next=raw_input("Masukan Key Contoh 6×13 = ")
+print "Hasil Penjumlahan = 65"
+
+next=raw_input("Ketik Yes Untuk Keluar= ")
+time.sleep(10)
+os.system("clear")
